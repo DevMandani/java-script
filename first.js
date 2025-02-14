b7559b788e2d4aa02bdc86e9e0fdf58c71254250
Dev_Mandani <devmandani2@gmail.com>
2025-02-14 19:35:36 +0530

    Update first.js

--- first.js
+++ first.js
@@ -42,3 +42,32 @@ console.log(year);
 console.log(typeof year);
 
 console.log(typeof null);
+
+
+let age = 31;
+age = 45;
+
+const birthyear = 1990;
+birthyear = 6757;
+
+const agejonas = 2037 - 1991;
+const agesarah = 2037 - 2020;
+console.log(agesarah);
+console.log(agejonas * 2);
+console.log(agejonas);
+
+
+const name = "jonas";
+const lastname = "dev";
+const space = " ";
+
+console.log(name + space + lastname);
+
+let x = 10 + 5;
+console.log(x);
+
+//comparision operator
+
+console.log(agejonas > agesarah);
+
+console.log(agesarah >= 18);
