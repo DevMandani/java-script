ce6955a61b4af630055f7ee7d8aa445b4d2e1536
Dev_Mandani <devmandani2@gmail.com>
2025-02-14 17:14:03 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,44 @@
+
+let js = "amazing";
+
+let firstname = "dev";
+
+console.log(firstname);
+
+
+console.log(40 + 23 + 8 - 10);
+let PI = 3.1415;
+
+let myfirstjob = "programmer";
+
+let mycurrentjob = "student";
+
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myfirstjob);
+console.log(mycurrentjob);
+
+let javascript = true;
+console.log(javascript);
+
+console.log(typeof javascript);
+console.log(typeof "jonas");
+
+console.log(typeof 34);
+
+javascript = "yes!";
+console.log(typeof javascript);
+
+let year;
+console.log(year);
+
+console.log(typeof year);
+
+year = 19191;
+
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
---
+++ second.js
@@ -0,0 +1,7 @@
+let country = "gujarat";
+let contient = "India";
+let population = "5M";
+
+console.log(country);
+console.log(contient);
+console.log(population);
