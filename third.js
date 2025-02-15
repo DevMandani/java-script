01b4538012fb7fd32f2cf7c039d8a3e1c9cdcb03
Dev_Mandani <devmandani2@gmail.com>
2025-02-15 08:17:56 +0530

    Add files via upload

---
+++ third.js
@@ -0,0 +1,14 @@
+const firstname1 = "dev";
+const job1 = 'student';
+const birthyear1 = 2006;
+const year1 = 2025;
+
+const dev = "i'm " + firstname1 + ',a ' + (year1 - birthyear1) + "years old" + job1 + "!";
+console.log(dev);
+
+const devnew = `I'm ${firstname1} , a ${year1 - birthyear1} year old ${job1}!`;
+console.log(devnew);
+
+console.log(`string
+ with 
+multiple lines`)
